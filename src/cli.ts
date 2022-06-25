524d5678382d7a0a03e2f9d5ee0191aa6bdafd0b
litwak913 <litwak913@qq.com>
2022-06-25 08:05:23 +0800

    add locale

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,47 @@
+import i18next from "i18next" 
+const i=i18next
 import inquirer from "inquirer"
-inquirer.prompt([
+import I18nextCLILanguageDetector from "i18next-cli-language-detector"
+import zh from "./locale/zh.json" assert {type: "json"}
+import en from "./locale/en.json" assert {type: "json"}
+i.use(I18nextCLILanguageDetector).init({
+    fallbackLng: "en",
+    resources: {
+        "en": {
+            translation: en
+        },
+        "zh": {
+            translation: zh
+        }
 
-])
+    }
+})
+console.log(i.t("hint"))
+const answer = await inquirer.prompt([
+    {
+        name:"input_file_path",
+        type:"input",
+        message:i.t("input_path")
+    },
+    {
+        name:"mode",
+        type:"list",
+        message:i.t("mode"),
+        choices:[
+            {name:i.t("obfuscator"),value:"obfuscator"},
+            {name:i.t("deobfuscator"),value:"deobfuscator"}
+        ],
+        default:0
+    },
+    {
+        name:"type",
+        type:"list",
+        message:i.t("type"),
+        default:0,
+        choices:[
+            {name:i.t("type_img"),value:"img"},
+            {name:i.t("type_file"),value:"file"}
+        ]
+    }
+]);
+console.log(answer)
